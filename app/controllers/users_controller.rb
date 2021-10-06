f7fae0e86412c1918520aca1cad9006abedbeb34
akiho19009 <mte.bn.1537.g@gmail.com>
2021-10-06 16:50:51 +0000

    [Finish]応用課題4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,10 +27,29 @@ class UsersController < ApplicationController
       render "edit"
     end
   end
+  
+  def follows
+    user = User.find(params[:id])
+    @users = user.followers
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers
+  end
+
+  
 
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
-
+  
+  def correct_user
+    user = User.find(params[:id])
+    if current_user != user
+      redirect_to user_path(current_user.id)
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,38 @@
+
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
   resources :books do
     resources :book_comments,only: [:create, :destroy]
     resource :favorites,only: [:create, :destroy]
   end
 end
+
+
+
+
+
+
+# Rails.application.routes.draw do
+#   devise_for :users
+#   root 'homes#top'
+#   get 'home/about' => 'homes#about'
+#   resources :users,only: [:show,:index,:edit,:update]
+#   resources :books do
+#     resources :book_comments,only: [:create, :destroy]
+#     resource :favorites,only: [:create, :destroy]
+#     resources :users, only: [:index, :show, :edit, :update] do
+# # ——————————————— ここから ———————————————
+#   resource :relationships, only: [:create, :destroy]
+#   get 'followings' => 'relationships#followings', as: 'followings'
+#   get 'followers' => 'relationships#followers', as: 'followers'
+# # ——————————— ここまでネストさせる ———————————
+#   end
+# end
+# end
